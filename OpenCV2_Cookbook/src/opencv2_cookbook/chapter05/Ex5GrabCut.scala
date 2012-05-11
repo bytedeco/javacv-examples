203905aa3fc845d9348ba61e36e4d5a45b7cc485
Jarek <jsacha@users.sf.net>
2012-05-11 00:10:10 -0400

    Add example for GrabCut segmentation from chapter 5.

---
+++ OpenCV2_Cookbook/src/opencv2_cookbook/chapter05/Ex5GrabCut.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2011-2012 Jarek Sacha. All Rights Reserved.
+ *
+ * Author's e-mail: jarek.listy at gmail.com
+ */
+
+package opencv2_cookbook.chapter05
+
+import java.io.File
+import com.googlecode.javacv.cpp.opencv_core._
+import com.googlecode.javacv.cpp.opencv_highgui._
+import com.googlecode.javacv.cpp.opencv_imgproc._
+import opencv2_cookbook.OpenCVUtils._
+
+
+/**
+ * Example from section "Extracting foreground objects with the GrabCut algorithm".
+ */
+object Ex5GrabCut extends App {
+
+    // Open image
+    val image = loadAndShowOrExit(new File("data/group.jpg"), CV_LOAD_IMAGE_COLOR)
+
+    // Define bounding rectangle, pixels outside this rectangle will be labeled as background.
+    val rectangle = new CvRect(10, 100, 380, 180)
+
+    val result = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1 /* channels */)
+    val iterCount = 5
+    val mode = GC_INIT_WITH_RECT
+
+    // GrabCut segmentation
+    grabCut(image, result, rectangle, null, null, iterCount, mode)
+
+    // Extract foreground mask
+    cvCmpS(result, GC_PR_FGD, result, CV_CMP_EQ)
+    show(result, "Result foreground mask")
+}
