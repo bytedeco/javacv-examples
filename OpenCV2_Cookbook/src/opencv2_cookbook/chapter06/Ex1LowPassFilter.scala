78013d12e34086737dff618114e29370d4d4727b
Jarek <jsacha@users.sf.net>
2012-05-15 21:05:31 -0400

    Add example 1 - low pass filtering with a Gaussian.

---
+++ OpenCV2_Cookbook/src/opencv2_cookbook/chapter06/Ex1LowPassFilter.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011-2012 Jarek Sacha. All Rights Reserved.
+ *
+ * Author's e-mail: jarek.listy at gmail.com
+ */
+
+package opencv2_cookbook.chapter06
+
+import opencv2_cookbook.OpenCVUtils._
+import java.io.File
+import com.googlecode.javacv.cpp.opencv_highgui._
+import com.googlecode.javacv.cpp.opencv_imgproc._
+import com.googlecode.javacv.cpp.opencv_core._
+
+/**
+ * The example for section "Filtering images using low-pass filters" in Chapter 6, page 142.
+ * Basic use of a Gaussian filter.
+ */
+object Ex1LowPassFilter extends App {
+
+    // Read input image
+    val src = loadAndShowOrExit(new File("data/boldt.jpg"), CV_LOAD_IMAGE_GRAYSCALE)
+
+    // Blur with a Gaussian filter
+    val dest = cvCreateImage(cvGetSize(src), src.depth, 1)
+    val kernelSize = new CvSize(5, 5)
+    val sigma = 1.5
+    val borderType = BORDER_DEFAULT
+    GaussianBlur(src, dest, kernelSize, sigma, sigma, borderType)
+    show(dest, "Blured")
+}
