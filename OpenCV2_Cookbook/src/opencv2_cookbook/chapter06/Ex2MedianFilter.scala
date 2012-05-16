bf3080599cf8004af5c3fd7a34b6e154b0a82945
Jarek <jsacha@users.sf.net>
2012-05-15 21:39:05 -0400

    Correct comments.

--- OpenCV2_Cookbook/src/opencv2_cookbook/chapter06/Ex2MedianFilter.scala
+++ OpenCV2_Cookbook/src/opencv2_cookbook/chapter06/Ex2MedianFilter.scala
@@ -13,12 +13,11 @@ import java.io.File
 import opencv2_cookbook.OpenCVUtils._
 
 /**
- * The example for section "Filtering images using a median filters" in Chapter 6, page 147.
- * Basic use of a median filter.
+ * The example for section "Filtering images using a median filter" in Chapter 6, page 147.
  */
 object Ex2MedianFilter extends App {
 
-    // Read input image with salt noise
+    // Read input image with a salt noise
     val src = loadAndShowOrExit(new File("data/boldt_salt.jpg"), CV_LOAD_IMAGE_GRAYSCALE)
 
     // Remove noise with a median filter
@@ -28,7 +27,7 @@ object Ex2MedianFilter extends App {
     show(dest, "Median filtered")
 
     // Since median filter really cleans up outlier with values above (salt) and below (pepper),
-    // we can reconstruct dark pixels that are most likely not effected by the noise.
+    // in this case, we can reconstruct dark pixels that are most likely not effected by the noise.
     val dest2 = cvCreateImage(cvGetSize(src), src.depth, 1)
     cvMin(src, dest, dest2)
     show(dest2, "Median filtered + dark pixel recovery")
