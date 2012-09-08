7fcafa457e2257d9d4899d1856ddebe1bcce9a87
Jarek Sacha <jsacha@users.sf.net>
2012-09-08 08:38:21 -0400

    Minor code style corrections.

--- OpenCV2_Cookbook/src/opencv2_cookbook/chapter03/ColorDetector.scala
+++ OpenCV2_Cookbook/src/opencv2_cookbook/chapter03/ColorDetector.scala
@@ -6,39 +6,44 @@
 
 package opencv2_cookbook.chapter03
 
-import opencv2_cookbook.OpenCVImageJUtils._
 import com.googlecode.javacv.cpp.opencv_core.IplImage
 import ij.process.ByteProcessor
 import math._
+import opencv2_cookbook.OpenCVImageJUtils._
 
 
-/**
- * Example of using a strategy pattern in algorithm design.
- * The pattern encapsulates an algorithm into a separate class.
- * To run this example use [[opencv2_cookbook.chapter03.Ex1ColorDetector]].
- *
- * The algorithm converts the input image to a binary by checking is pixel color is within a given distance from a desired color.
- * Pixels with color close to the desired color are white, other black.
- *
- * This Scala code functionally is equivalent to C++ code in chapter 3 section
- * "Using the Strategy pattern in algorithm design".
- * The original example in the book is using "C++ API". To make operations on image pixels easier and more efficient
- * OpenCV color image is converted to ImageJ representation during processing.
- *
- * Unlike the in the C++ example, this class does not pre-allocates and hold space for process image,
- * it is create only when needed.
- */
+/** Example of using a strategy pattern in algorithm design.
+  *
+  * The pattern encapsulates an algorithm into a separate class.
+  * To run this example use [[opencv2_cookbook.chapter03.Ex1ColorDetector]].
+  *
+  * The algorithm converts the input image to a binary by checking is pixel color is within a given distance from
+  * a desired color.
+  * Pixels with color close to the desired color are white, other black.
+  *
+  * This Scala code functionally is equivalent to C++ code in chapter 3 section
+  * "Using the Strategy pattern in algorithm design".
+  * The original example in the book is using "C++ API". To make operations on image pixels easier and more efficient
+  * OpenCV color image is converted to ImageJ representation during processing.
+  *
+  * Unlike the in the C++ example, this class does not pre-allocates and hold space for process image,
+  * it is create only when needed.
+  */
 class ColorDetector(private var _minDist: Int = 100,
                     private var _target: ColorRGB = ColorRGB(130, 190, 230)) {
 
 
-    def colorDistanceThreshold = _minDist
+    def colorDistanceThreshold: Int = _minDist
 
-    def colorDistanceThreshold_=(dist: Int) {_minDist = max(0, dist)}
 
-    def targetColor = _target
+    def colorDistanceThreshold_=(dist: Int) { _minDist = max(0, dist) }
 
-    def targetColor_=(color: ColorRGB) {_target = color}
+
+    def targetColor: ColorRGB = _target
+
+
+    def targetColor_=(color: ColorRGB) { _target = color }
+
 
     def process(iplImage: IplImage): IplImage = {
 
@@ -61,6 +66,7 @@ class ColorDetector(private var _minDist: Int = 100,
         // Convert back to OpenCV's IplImage
         IplImage.createFrom(toBufferedImage(dest))
     }
+
 
     @inline
     private def distance(color: java.awt.Color): Double = {
