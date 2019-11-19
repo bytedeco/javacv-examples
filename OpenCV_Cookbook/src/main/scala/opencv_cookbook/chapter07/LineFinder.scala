df0c449f756ab830267b1df82cfbe30a28d00e0d
Jarek Sacha <jpsacha@gmail.com>
2019-11-18 21:35:37 -0500

    Restore simple code after bug #717 is fixed

--- OpenCV_Cookbook/src/main/scala/opencv_cookbook/chapter07/LineFinder.scala
+++ OpenCV_Cookbook/src/main/scala/opencv_cookbook/chapter07/LineFinder.scala
@@ -7,7 +7,6 @@
 package opencv_cookbook.chapter07
 
 
-import org.bytedeco.javacpp.IntPointer
 import org.bytedeco.opencv.global.opencv_imgproc._
 import org.bytedeco.opencv.opencv_core._
 import org.bytedeco.opencv.opencv_imgproc._
@@ -51,10 +50,7 @@ class LineFinder(val deltaRho: Double = 1,
     */
   def drawDetectedLines(image: Mat): Unit = {
     for (i <- 0 until lines.size().toInt) {
-      // Due to bug #717 in OpenCV wrapper in v.1.5 for Vec4iVector, line segment coordinates cannot be extracted directly.
-      // A workaround is to wrap `FloatPointer` returned from `lines.get` in `IntPointer`,
-      // based on suggestion [[https://github.com/bytedeco/javacpp-presets/issues/717#issuecomment-485057141]]
-      val l = new IntPointer(lines.get(i))
+      val l = lines.get(i)
       val pt1 = new Point(l.get(0), l.get(1))
       val pt2 = new Point(l.get(2), l.get(3))
 
