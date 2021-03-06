3395fc716a4a37306a87b83a80d08a06fb9f5f78
Jarek Sacha <jpsacha@gmail.com>
2021-03-05 19:33:22 -0500

    [Cookbook] Use actual Gaussian filter

--- OpenCV_Cookbook/src/main/scala/opencv_cookbook/chapter06/Ex1LowPassFilter.scala
+++ OpenCV_Cookbook/src/main/scala/opencv_cookbook/chapter06/Ex1LowPassFilter.scala
@@ -6,29 +6,28 @@
 
 package opencv_cookbook.chapter06
 
-import java.io.File
-
 import opencv_cookbook.OpenCVUtils._
 import org.bytedeco.opencv.global.opencv_core._
 import org.bytedeco.opencv.global.opencv_imgcodecs._
 import org.bytedeco.opencv.global.opencv_imgproc._
 import org.bytedeco.opencv.opencv_core._
 
+import java.io.File
+
 /**
- * The example for section "Filtering images using low-pass filters" in Chapter 6, page 142.
- * Basic use of a Gaussian filter.
- */
+  * The example for section "Filtering images using low-pass filters" in Chapter 6, page 142.
+  * Basic use of a Gaussian filter.
+  */
 object Ex1LowPassFilter extends App {
 
   // Read input image
   val src = loadAndShowOrExit(new File("data/boldt.jpg"), IMREAD_GRAYSCALE)
 
   // Blur with a Gaussian filter
-  //    val dest = cvCreateImage(cvGetSize(src), src.depth, 1)
   val dest = new Mat()
   val kernelSize = new Size(5, 5)
   val sigma = 1.5
   val borderType = BORDER_DEFAULT
-  blur(src, dest, kernelSize)
+  GaussianBlur(src, dest, kernelSize, sigma)
   show(dest, "Blurred")
 }
